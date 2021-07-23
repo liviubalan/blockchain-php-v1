30b48a45593d7dd7873a299aaff81256726ed0d0
Liviu Balan <liv_romania@yahoo.com>
2021-07-23 17:14:27 +0300

    Create methods "getLastBlock" + "createNewTransaction"

--- www/blockchain.php
+++ www/blockchain.php
@@ -3,22 +3,41 @@
 class Blockchain
 {
     public array $chain = [];
-    public array $newTransactions = [];
+    public array $pendingTransactions = [];
 
     public function createNewBlock(int $nonce, string $previousBlockHash, string $hash): array
     {
         $newBlock = [
             'index' => count($this->chain) + 1,
             'timestamp' => time(),
-            'transactions' => $this->newTransactions,
+            'transactions' => $this->pendingTransactions,
             'nonce' => $nonce,
             'hash' => $hash,
             'previousBlockHash' => $previousBlockHash,
         ];
 
-        $this->newTransactions = [];
+        $this->pendingTransactions = [];
         $this->chain[] = $newBlock;
 
         return $newBlock;
+    }
+
+    public function getLastBlock(): array
+    {
+        $index = count($this->chain) - 1;
+
+        return $this->chain[$index];
+    }
+
+    public function createNewTransaction(float $amount, string $sender, string $recipient): int
+    {
+        $newTransaction = [
+            'amount' => $amount,
+            'sender' => $sender,
+            'recipient' => $recipient,
+        ];
+        $this->pendingTransactions[] = $newTransaction;
+
+        return $this->getLastBlock()['index'] + 1;
     }
 }
--- www/index.php
+++ www/index.php
@@ -1,13 +1,23 @@
 <?php
 
+error_reporting(-1);
+ini_set('display_errors', '1');
+
 require_once './blockchain.php';
 
 $bitcoin = new Blockchain();
 
 echo '<pre>';
-var_dump($bitcoin);
+
 $bitcoin->createNewBlock(1, 'hash-0', 'hash-1');
-$bitcoin->createNewBlock(2, 'hash-1', 'hash-2');
-$bitcoin->createNewBlock(3, 'hash-2', 'hash-3');
+
+$bitcoin->createNewTransaction(100, 'sender-1', 'recipient-1');
+$bitcoin->createNewBlock(2, 'hash-1', 'hash-2'); // mine a new block
+
+$bitcoin->createNewTransaction(50, 'sender-2', 'recipient-2');
+$bitcoin->createNewTransaction(300, 'sender-3', 'recipient-3');
+$bitcoin->createNewTransaction(2000, 'sender-4', 'recipient-4');
+$bitcoin->createNewBlock(3, 'hash-2', 'hash-3'); // mine a new block
+
 var_dump($bitcoin);
 echo '</pre>';
