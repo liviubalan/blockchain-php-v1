9700b9a1affc666845b02159204182ceb37db6e1
Liviu Balan <liv_romania@yahoo.com>
2021-07-23 22:42:49 +0300

    Fix vhost data

--- provision-shell/bootstrap.sh
+++ provision-shell/bootstrap.sh
@@ -3,5 +3,5 @@
 sudo apt-get update
 sudo apt-get -y install apache2
 sudo apt-get -y install php libapache2-mod-php php-mysql
-sudo mkdir /var/www/your_domain
 sudo rm -rf /var/www/html
+sudo ln -s /vagrant/www /var/www/html
