047eb5f4f77a8a78438cdc45d21bcb9dc305241a
Liviu Balan <liv_romania@yahoo.com>
2021-07-24 00:11:46 +0300

    Add Symfony 4.4
    URL:
    https://symfony.com/doc/4.4/setup.html
    https://stackoverflow.com/questions/38793676/php-xml-extension-not-installed
    
    CLI:
    php composer.phar create-project symfony/skeleton:"^4.4" sf44

--- provision-shell/bootstrap.sh
+++ provision-shell/bootstrap.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update
 sudo apt-get -y install apache2
-sudo apt-get -y install php libapache2-mod-php php-mysql
+sudo apt-get -y install php libapache2-mod-php php-xml
 sudo rm -rf /var/www/html
 sudo ln -s /vagrant/www /var/www/html
