193513b17a45577c2bd477febd3e722fcb4e023b
Liviu Balan <liv_romania@yahoo.com>
2021-07-23 12:21:21 +0300

    Install Apache + PHP
    URL:
    https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-on-ubuntu-20-04
    https://superuser.com/questions/164553/automatically-answer-yes-when-using-apt-get-install

--- provision-shell/bootstrap.sh
+++ provision-shell/bootstrap.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y install apache2
+sudo apt-get -y install php libapache2-mod-php php-mysql
+sudo mkdir /var/www/your_domain
+sudo rm -rf /var/www/html
