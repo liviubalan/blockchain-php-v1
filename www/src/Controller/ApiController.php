2d75f2f53d4a1ae654c8ebd1aec97bf6ad74ef8e
Liviu Balan <liv_romania@yahoo.com>
2021-07-24 16:43:50 +0300

    Cache "bitcoin" object

--- www/src/Controller/ApiController.php
+++ www/src/Controller/ApiController.php
@@ -3,17 +3,36 @@
 namespace App\Controller;
 
 use App\Classes\Blockchain;
+use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AbstractAdapter;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class ApiController extends AbstractController
 {
-    private $bitcoin;
+    const CACHE_KEY = 'bitcoin';
 
-    public function __construct()
+    private Blockchain $bitcoin;
+
+    /**
+     * @var AbstractAdapter
+     */
+    private $cache;
+
+    public function setContainer(ContainerInterface $container): ?ContainerInterface
     {
-        $this->bitcoin = new Blockchain();
+        $this->cache = $container->get('cache.app');
+
+        $cachedBitcoin = $this->cache->getItem(static::CACHE_KEY);
+        if ($cachedBitcoin->isHit()) {
+            $bitcoin = unserialize($cachedBitcoin->get());
+        } else {
+            $bitcoin = new Blockchain();
+        }
+        $this->bitcoin = $bitcoin;
+
+        return parent::setContainer($container);
     }
 
     public function blockchain(): JsonResponse
@@ -23,12 +42,34 @@ class ApiController extends AbstractController
 
     public function transaction(Request $request): JsonResponse
     {
-        $amount = $request->request->get('amount', 0);
-        $sender = $request->request->get('sender', '');
-        $recipient = $request->request->get('recipient', '');
+        $content = trim($request->getContent());
+        if (!$content) {
+            throw $this->createNotFoundException('Empty request');
+        }
+
+        $content = json_decode($content, true);
+        if (
+            !array_key_exists('amount', $content)
+            || !array_key_exists('sender', $content)
+            || !array_key_exists('recipient', $content)
+        ) {
+            throw $this->createNotFoundException('The following fields are mandatory: amount, sender, recipient.');
+        }
+
+        $amount = (float) $content['amount'];
+        $sender = $content['sender'];
+        $recipient = $content['recipient'];
 
         $blockIndex = $this->bitcoin->createNewTransaction($amount, $sender, $recipient);
 
         return new JsonResponse("Transaction will be added in block $blockIndex.");
+    }
+
+    public function __destruct()
+    {
+        $cachedBitcoin = $this->cache->getItem(static::CACHE_KEY);
+        $serializedBitcoin = serialize($this->bitcoin);
+        $cachedBitcoin->set($serializedBitcoin);
+        $this->cache->save($cachedBitcoin);
     }
 }
