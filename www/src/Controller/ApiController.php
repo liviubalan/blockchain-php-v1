05adb4fe1ce543613193226b2ba81beacb259711
Liviu Balan <liv_romania@yahoo.com>
2021-07-24 13:09:02 +0300

    Create route "/transaction"

--- www/src/Controller/ApiController.php
+++ www/src/Controller/ApiController.php
@@ -4,17 +4,31 @@ namespace App\Controller;
 
 use App\Classes\Blockchain;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class ApiController extends AbstractController
 {
-    private $blockchain;
+    private $bitcoin;
 
-    public function blockchain()
+    public function __construct()
     {
-        $this->blockchain = new Blockchain();
-        var_dump($this->blockchain);
+        $this->bitcoin = new Blockchain();
+    }
 
-        die('tst');
+    public function blockchain(): JsonResponse
+    {
+        return new JsonResponse($this->bitcoin);
+    }
+
+    public function transaction(Request $request): JsonResponse
+    {
+        $amount = $request->request->get('amount', 0);
+        $sender = $request->request->get('sender', '');
+        $recipient = $request->request->get('recipient', '');
+
+        $blockIndex = $this->bitcoin->createNewTransaction($amount, $sender, $recipient);
+
+        return new JsonResponse("Transaction will be added in block $blockIndex.");
     }
 }
