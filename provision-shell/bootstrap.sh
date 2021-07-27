2a082cfb7747d3b012eeba280c14bdac5460222f
Liviu Balan <liv_romania@yahoo.com>
2021-07-27 13:09:11 +0300

    Overwrite default Apache virtual host

--- provision-shell/bootstrap.sh
+++ provision-shell/bootstrap.sh
@@ -6,6 +6,7 @@ sudo apt-get -y install php libapache2-mod-php php-xml
 sudo rm -rf /var/www/blockchain
 sudo ln -s /vagrant/www /var/www/blockchain
 
+sudo cp /vagrant/provision-shell/000-default.conf /etc/apache2/sites-available/000-default.conf
 sudo cp /vagrant/provision-shell/blockchain.conf /etc/apache2/sites-available/blockchain.conf
 HOST=$(hostname)
 sudo sed -i "s/nodeHost/$HOST/" /etc/apache2/sites-available/blockchain.conf
